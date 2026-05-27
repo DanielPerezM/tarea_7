f40db9de2e44f2eafbec55fa380dfc03f58f78f4
DanielPerezM <19031059@gmail.com>
2026-05-26 18:10:36 -0600

    Cambiamos el estilo de NavBar,

--- lib/widgets/NavBar.dart
+++ lib/widgets/NavBar.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
-import 'package:line_icons/line_icons.dart';
 import 'package:provider/provider.dart';
 import 'package:tarea_7/listeners/provider_nav.dart';
 
@@ -11,41 +9,133 @@ class NavBarW extends StatelessWidget {
   Widget build(BuildContext context) {
     final navProvider = Provider.of<NavProvider>(context);
 
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(blurRadius: 20, color: Colors.black.withOpacity(.1)),
-        ],
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 10),
+        child: Container(
+          height: 70,
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          decoration: BoxDecoration(
+            color: Colors.orange.shade900,
+            borderRadius: BorderRadius.circular(40),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(.25),
+                blurRadius: 20,
+                offset: const Offset(0, 8),
+              ),
+            ],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              _NavItem(
+                index: 0,
+                icon: Icons.home_outlined,
+                title: 'Home',
+                selectedIndex: navProvider.selectedIndex,
+                onTap: navProvider.changeScreen,
+              ),
+              _NavItem(
+                index: 1,
+                icon: Icons.route_outlined,
+                title: 'Rutas',
+                selectedIndex: navProvider.selectedIndex,
+                onTap: navProvider.changeScreen,
+              ),
+              _NavItem(
+                index: 2,
+                icon: Icons.search,
+                title: 'Buscar',
+                selectedIndex: navProvider.selectedIndex,
+                onTap: navProvider.changeScreen,
+              ),
+              _NavItem(
+                index: 3,
+                icon: Icons.person_outline,
+                title: 'Perfil',
+                selectedIndex: navProvider.selectedIndex,
+                onTap: navProvider.changeScreen,
+              ),
+              _NavItem(
+                index: 4,
+                icon: Icons.workspace_premium_outlined,
+                title: 'Premium',
+                selectedIndex: navProvider.selectedIndex,
+                onTap: navProvider.changeScreen,
+              ),
+              _NavItem(
+                index: 5,
+                icon: Icons.account_balance_wallet_outlined,
+                title: 'Recargas',
+                selectedIndex: navProvider.selectedIndex,
+                onTap: navProvider.changeScreen,
+              ),
+            ],
+          ),
+        ),
       ),
-      child: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
-          child: SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: GNav(
-              selectedIndex: navProvider.selectedIndex,
-              rippleColor: Colors.grey[300]!,
-              hoverColor: Colors.grey[100]!,
-              gap: 8,
-              activeColor: Colors.orange.shade900,
-              iconSize: 24,
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-              duration: const Duration(milliseconds: 400),
-              tabBackgroundColor: Colors.orange.shade100,
-              color: Colors.black,
-              onTabChange: (index) {
-                navProvider.changeScreen(index);
-              },
-              tabs: const [
-                GButton(icon: LineIcons.home, text: 'Home'),
-                GButton(icon: LineIcons.route, text: 'Rutas'),
-                GButton(icon: LineIcons.search, text: 'Buscar'),
-                GButton(icon: LineIcons.user, text: 'Perfil'),
-                GButton(icon: LineIcons.creditCard, text: 'Premium'),
-                GButton(icon: LineIcons.wallet, text: 'Recargas'),
-              ],
-            ),
+    );
+  }
+}
+
+class _NavItem extends StatelessWidget {
+  final int index;
+  final IconData icon;
+  final String title;
+  final int selectedIndex;
+  final Function(int) onTap;
+
+  const _NavItem({
+    required this.index,
+    required this.icon,
+    required this.title,
+    required this.selectedIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isSelected = selectedIndex == index;
+
+    return Expanded(
+      child: InkWell(
+        borderRadius: BorderRadius.circular(30),
+        onTap: () => onTap(index),
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 300),
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
+          decoration: BoxDecoration(
+            color: isSelected
+                ? Colors.white.withOpacity(.18)
+                : Colors.transparent,
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                icon,
+                color: isSelected ? Colors.white : Colors.white.withOpacity(.6),
+                size: isSelected ? 27 : 24,
+              ),
+              const SizedBox(height: 3),
+              Flexible(
+                child: Text(
+                  title,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    color: isSelected
+                        ? Colors.white
+                        : Colors.white.withOpacity(.6),
+                    fontSize: 10,
+                    fontWeight: isSelected
+                        ? FontWeight.bold
+                        : FontWeight.normal,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
